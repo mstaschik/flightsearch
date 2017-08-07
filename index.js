627a6c686178f0b9a6e2eb2157165fd21383f549
mstaschik <marco.staschik@gmail.com>
2017-08-08 00:00:13 +0200

    commi2

--- index.js
+++ index.js
@@ -12,6 +12,53 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
+
+     // create http request client to consume the QPX API
+ var request = require("request")
+
+    // JSON to be passed to the QPX Express API
+    var requestData = {
+        "request": {
+            "slice": [
+            {
+                "origin": "ZRH",
+                "destination": "DUS",
+                "date": "2018-12-02"
+            }
+            ],
+            "passengers": {
+                "adultCount": 1,
+                "infantInLapCount": 0,
+                "infantInSeatCount": 0,
+                "childCount": 0,
+                "seniorCount": 0
+            },
+            "solutions": 2,
+            "refundable": false
+        }
+    }
+
+    // QPX REST API URL (I censored my api key)
+    url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBB9Q3zr7-Mp1uOYA3y8unPCOyPsjS7qBg"
+
+// fire request
+request({
+    url: url,
+    method: "POST",
+    json: requestData
+}, function (error, response, bodyl) {
+    if (!error && response.statusCode === 200) {
+        console.log(bodyl)
+    }
+    else {
+        console.log("error: " + error)
+        console.log("response.statusCode: " + response.statusCode)
+        console.log("response.statusText: " + response.statusText)
+    }
+})
+
+
+
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
