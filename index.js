6fb526b46778dca7cdfc7f86ef3dbb3c7cf06e28
mstaschik <marco.staschik@gmail.com>
2017-08-08 00:28:47 +0200

    commit2

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
+
     var API = require('qpx-express');
+<<<<<<< HEAD
  
 var apiKey = 'AIzaSyBB9Q3zr7-Mp1uOYA3y8unPCOyPsjS7qBg';
 var qpx = new API(apiKey);
@@ -39,8 +41,37 @@ var requestData = {
     };
  
 qpx.getInfo(requestData, function(error, data){
+=======
+
+    var apiKey = 'AIzaSyBB9Q3zr7-Mp1uOYA3y8unPCOyPsjS7qBg';
+    var qpx = new API(apiKey);
+
+    var body = {
+      "request": {
+        "slice": [
+        {
+            "origin": "ZRH",
+            "destination": "DUS",
+            "date": "2018-12-02"
+        }
+        ],
+        "passengers": {
+            "adultCount": 1,
+            "infantInLapCount": 0,
+            "infantInSeatCount": 0,
+            "childCount": 0,
+            "seniorCount": 0
+        },
+        "solutions": 2,
+        "refundable": false
+    }
+};
+
+qpx.getInfo(body, function(error, data){
+>>>>>>> origin/Marco
     //console.log('Heyy!', data);
 });
+
 
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
@@ -48,6 +79,7 @@ qpx.getInfo(requestData, function(error, data){
         displayText: body,
         source: 'webhook-echo-sample'
     });
+
 });
 
 restService.listen((process.env.PORT || 8000), function() {
