2fa52b1790a3f598cbe900f9b0fd7e73576a5c8a
mstaschik <marco.staschik@gmail.com>
2017-08-08 00:18:40 +0200

    commit2

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-
-     // create http request client to consume the QPX API
-     var request = require("request")
-
-    // JSON to be passed to the QPX Express API
-    var requestData = {
-        "request": {
+    var API = require('qpx-express');
+ 
+var apiKey = 'AIzaSyBB9Q3zr7-Mp1uOYA3y8unPCOyPsjS7qBg';
+var qpx = new API(apiKey);
+ 
+var body = {
+  "request": {
             "slice": [
             {
                 "origin": "ZRH",
@@ -36,36 +36,19 @@ restService.post('/echo', function(req, res) {
             "solutions": 2,
             "refundable": false
         }
-    }
-
-    // QPX REST API URL (I censored my api key)
-    url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBB9Q3zr7-Mp1uOYA3y8unPCOyPsjS7qBg"
-
-// fire request
-request({
-    url: url,
-    method: "POST",
-    json: requestData
-}, function (error, response, bodyl) {
-    if (!error && response.statusCode === 200) {
-        console.log(bodyl)
-
-        var speech = req.bodyl.result && req.bodyl.result.parameters && req.bodyl.result.parameters.echoText ? req.bodyl.result.parameters.echoText : "Seems like some problem. Speak again."
-        return res.json({
-            speech: bodyl,
-            displayText: bodyl,
-            source: 'webhook-echo-sample'
-        });
-    }
-    else {
-        console.log("error: " + error)
-        console.log("response.statusCode: " + response.statusCode)
-        console.log("response.statusText: " + response.statusText)
-    }
-})
+    };
+ 
+qpx.getInfo(body, function(error, data){
+    console.log('Heyy!', data);
+});
 
 
-
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    return res.json({
+        speech: body,
+        displayText: body,
+        source: 'webhook-echo-sample'
+    });
 });
 
 restService.listen((process.env.PORT || 8000), function() {
