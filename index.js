dfe83387952b770c2e079ebbc82cc3cbf4e10b2b
mstaschik <marco.staschik@gmail.com>
2017-08-08 00:24:43 +0200

    commit2

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ qpx.getInfo(body, function(error, data){
     console.log('Heyy!', data);
 });
 
-
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: body,
