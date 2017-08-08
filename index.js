81045b9abdf242c9e86469964961ca0e33f4622d
mstaschik <marco.staschik@gmail.com>
2017-08-08 17:53:30 +0200

    comit

--- index.js
+++ index.js
@@ -12,39 +12,6 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 
-const API = require('qpx-express');
-
-var apiKey = 'AIzaSyBB9Q3zr7-Mp1uOYA3y8unPCOyPsjS7qBg';
-var qpx = new API(apiKey);
-
-
-var requestedData = {
-  "request": {
-    "slice": [
-    {
-        "origin": "ZRH",
-        "destination": "DUS",
-        "date": "2018-12-02"
-    }
-    ],
-    "passengers": {
-        "adultCount": 1,
-        "infantInLapCount": 0,
-        "infantInSeatCount": 0,
-        "childCount": 0,
-        "seniorCount": 0
-    },
-    "solutions": 2,
-    "refundable": false
-}
-};
-
-qpx.getInfo(requestedData, function(error, data){
-    console.log('Heyy!', data);
-
-});
-
-
 restService.post('/echo', function(req, res) {
 
 
