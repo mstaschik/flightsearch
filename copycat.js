d448ea86e048f3d1ee1268c1008a7dbf5d318292
mstaschik <marco.staschik@gmail.com>
2017-08-13 23:06:10 +0200

    1

--- copycat.js
+++ copycat.js
@@ -12,7 +12,7 @@ const qpx = new API(apiKey);
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
-    extended: true
+	extended: true
 }));
 
 restService.use(bodyParser.json());
@@ -20,18 +20,18 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
 
 
-var origin = req.body.result.parameters['Flughafen1'];
-var destination = req.body.result.parameters['Flughafen2']
-var departureDate = req.body.result.parameters['date']
+	var origin = req.body.result.parameters['Flughafen1'];
+	var destination = req.body.result.parameters['Flughafen2']
+	var departureDate = req.body.result.parameters['date']
 
-var body = {
-	"request": {
-		"passengers": { 
-			"adultCount": 1 
-		},
-		"slice": [{
-			"origin": origin,
-			"destination": destination,
+	var body = {
+		"request": {
+			"passengers": { 
+				"adultCount": 1 
+			},
+			"slice": [{
+				"origin": origin,
+				"destination": destination,
             "date": departureDate // YYYY-MM-DD 
         }
         ],
@@ -42,27 +42,35 @@ var body = {
 
 function qpxFunction() {
 	var flightdates = "Abflug: " + origin + " Ziel: " + destination + " Datum: " + departureDate;
-    return flightdates;
+	return flightdates;
 }
 
 qpx.getInfo(body, function (error, data){
 	    //console.log('Heyy!', data);
 
-		  for(var i = 0; i < data.trips.tripOption.length; i++) {
+	    for(var i = 0; i < data.trips.tripOption.length; i++) {
 		       //JSON.stringify(data.trips.tripOption[index].pricing[0].saleTotal);
 
-		        var price = data.trips.tripOption[i].pricing[0].saleTotal;
-		        var carrier = data.trips.tripOption[i].pricing[0].fare[0].carrier;
-		        console.log(carrier + ": " + price);
-		  }
-});
+		       var price = data.trips.tripOption[i].pricing[0].saleTotal;
+		       var carrier = data.trips.tripOption[i].pricing[0].fare[0].carrier;
+		       console.log(carrier + ": " + price);
+		   }
+		   let msg = data.trips.tripOption[0].pricing[0].fare[0].carrier
+		   
+		   return res.json({
+		   	speech: msg,
+		   	displayText: msg,
+		   	source: 'webhook-echo-sample'
+		   });
 
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+		});
+
+    //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     
     return res.json({
-        speech: qpxFunction(),
-        displayText: qpxFunction(),
-        source: 'webhook-echo-sample'
+    	speech: qpxFunction(),
+    	displayText: qpxFunction(),
+    	source: 'webhook-echo-sample'
     });
 
 
@@ -74,5 +82,5 @@ qpx.getInfo(body, function (error, data){
 
 
 restService.listen((process.env.PORT || 8000), function() {
-    console.log("Server up and listening");
+	console.log("Server up and listening");
 });
