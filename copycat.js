255510825253aab9ef0352b56c7b42c3033c331b
mstaschik <marco.staschik@gmail.com>
2017-08-13 20:29:49 +0200

    a

--- copycat.js
+++ copycat.js
@@ -56,10 +56,10 @@ qpx.getInfo(body, function (error, data){
 
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     
-
+var lol = qpxFunction();
     return res.json({
-        speech: qpxFunction();,
-        displayText: qpxFunction();,
+        speech: qpxFunction(),
+        displayText: lol,
         source: 'webhook-echo-sample'
     });
 
