4286fd6471b6e663f6254fd24723237878b15985
mstaschik <marco.staschik@gmail.com>
2017-08-13 19:04:09 +0200

    c

---
+++ copycat.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const API = require('qpx-express');
+
+const apiKey = 'AIzaSyBB9Q3zr7-Mp1uOYA3y8unPCOyPsjS7qBg';
+const qpx = new API(apiKey);
+
+
+const restService = express();
+
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+restService.use(bodyParser.json());
+
+restService.post('/echo', function(req, res) {
+
+var origin = "LHR"
+var destination = "LAX"
+var departureDate = "2018-01-08"
+
+var body = {
+	"request": {
+		"passengers": { 
+			"adultCount": 1 
+		},
+		"slice": [{
+			"origin": origin,
+			"destination": destination,
+            "date": departureDate // YYYY-MM-DD 
+        }
+        ],
+        "solutions": 5
+    }
+};
+
+qpx.getInfo(body, function(error, data){
+	    //console.log('Heyy!', data);
+
+  for(var i = 0; i < data.trips.tripOption.length; i++) {
+       //JSON.stringify(data.trips.tripOption[index].pricing[0].saleTotal);
+
+        var price = data.trips.tripOption[i].pricing[0].saleTotal;
+        var carrier = data.trips.tripOption[i].pricing[0].fare[0].carrier;
+        console.log(carrier + ": " + price);
+  }
+});
+
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    return res.json({
+        speech: origin,
+        displayText: origin,
+        source: 'webhook-echo-sample'
+    });
+});
+
+
+
+
+
+
+restService.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
