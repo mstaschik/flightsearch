67ae183aa4309dee90849b8501fffd0bb4779294
mstaschik <marco.staschik@gmail.com>
2017-08-14 18:03:30 +0200

    1

--- copycat_2.js
+++ copycat_2.js
@@ -100,9 +100,7 @@ restService.post('/echo', function(req, res) {
 			   resolve(output);
 
 			});
-    		res.on('error', (error) => {
-    			reject(error);
-    		});
+
     //var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
 		});
 
